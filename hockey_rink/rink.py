d7f5e63ce70750f3a93d536a6bd18ced32026e7d
VKarpick <vincentkarpick@gmail.com>
2021-05-12 22:58:48 -0700

    Use URL as path to image

--- hockey_rink/rink.py
+++ hockey_rink/rink.py
@@ -634,7 +634,8 @@ class NWHLRink(NHLRink):
             "center_circle": {"thickness": 2, "color": "#003366", "zorder": 12},
             "center_dot": {"visible": False},
             "logo": {"class": rf.CircularImage,
-                     "path": "images/nwhl_logo.jpg", "radius": center_radius,
+                     "path": "https://github.com/the-bucketless/hockey_rink/blob/master/images/nwhl_logo.png",
+                     "radius": center_radius,
                      "is_constrained": False, "zorder": 11}
         }
 
